92c211a3a0929b599c40477fd08256725c44e441
Jill-Jênn Vie <vie@jill-jenn.net>
2013-11-15 10:46:55 +0100

    Add Stanford codes

--- code/suffix.cpp
+++ code/suffix.cpp
@@ -2,17 +2,17 @@
 #include <tuple>
 using namespace std;
 
-int main() {
-	string s;
-	cin >> s;
+const int inf = 40000;
+const int log_inf = 17;
+
+void suffix_tree(string s, int p[log_inf][inf], int &k) {
 	int N = s.length();
-	int p[17][40000];
 	for(int i = 0 ; i < N ; i++)
 		p[0][i] = s[i] - 'a';
-	copy(p[0], p[0] + N, ostream_iterator<int>(cout, " "));
-	cout << endl;
-	tuple<int, int, int> v[40000];
-	int k = 0;
+	// copy(p[0], p[0] + N, ostream_iterator<int>(cout, " "));
+	// cout << endl;
+	tuple<int, int, int> v[inf];
+	k = 0;
 	for(int shift = 1 ; shift >> 1 < N ; shift <<= 1) {
 		k++;
 		for(int i = 0 ; i < N ; i++)
@@ -21,15 +21,26 @@ int main() {
 		for(int i = 0 ; i < N ; i++)
 			p[k][get<2>(v[i])] = (i > 0 && get<0>(v[i]) == get<0>(v[i - 1])
 				&& get<1>(v[i]) == get<1>(v[i - 1])) ? p[k][get<2>(v[i - 1])] : i;
-		copy(p[k], p[k] + N, ostream_iterator<int>(cout, " "));
-		cout << endl;
+		// copy(p[k], p[k] + N, ostream_iterator<int>(cout, " "));
+		// cout << endl;
 	}
-	int w[40000];
-	for(int i = 0 ; i < N ; i++)
+}
+
+void lcp(int x, int y) {
+	
+}
+
+int main() {
+	string s;
+	cin >> s;
+	int k, p[log_inf][inf];
+	suffix_tree(s, p, k);
+	int w[inf];
+	for(int i = 0 ; i < s.length() ; i++)
 		w[p[k][i]] = i;
-	for(int i = 0 ; i < N ; i++)
+	for(int i = 0 ; i < s.length() ; i++)
 		cout << w[i] << " ";
 	cout << endl;
-	for(int i = 0 ; i < N ; i++)
+	for(int i = 0 ; i < s.length() ; i++)
 		cout << s.substr(w[i]) << endl;
 }
